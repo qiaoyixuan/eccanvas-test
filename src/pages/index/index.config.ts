78ddf48ad9916efa39c1ff534258c269ab397616
代码大魔王 <qiaoyixuan@bytedance.com>
2021-09-30 18:20:15 +0800

    init

---
+++ src/pages/index/index.config.ts
@@ -0,0 +1,9 @@
+export default {
+  navigationBarTitleText: '首页',
+  usingComponents: {
+    // 定义需要引入的第三方组件
+    // 1. key 值指定第三方组件名字，以小写开头
+    // 2. value 值指定第三方组件 js 文件的相对路径
+    'ec-canvas': '../../components/ec-canvas/ec-canvas'
+  }
+}
